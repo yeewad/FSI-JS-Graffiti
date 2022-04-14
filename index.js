aedf571f1180bc3bbfb5cbf34f3a7c6f5037587d
Yeewa Dansiri <yeewa.honeyman@gmail.com>
2022-04-13 19:27:33 -0700

    format webpage

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Select some elements...
+let banner = document.getElementById('page-header')
+banner.style.backgroundColor = 'orange'
+banner.style.textAlign = 'left'
+banner.style.fontFamily = 'Georgia'
+
+    let frame = document.querySelectorAll('.dog-image')
+for (let x = 0; x < frame.length; x++) {
+    frame[x].style.borderColor = 'orange'
+    frame[x].style.borderStyle = 'double'
+}
